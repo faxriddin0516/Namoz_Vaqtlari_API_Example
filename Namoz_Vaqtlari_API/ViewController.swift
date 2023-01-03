362170379f750d387b2b73795e4b7783504ec8e6
faxriddin0516 <faxaoff@gmail.com>
2023-01-03 16:20:53 +0500

    Initial Commit

---
+++ Namoz_Vaqtlari_API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Namoz_Vaqtlari_API
+//
+//  Created by MacAir on 03/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
